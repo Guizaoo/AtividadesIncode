0a9d293917e6f517b783993ca1f414dc46af3a5b
Guizaoo <Guilhermexxzz54@gmail.com>
2025-02-11 12:07:05 -0300

    adicao de atv

--- src/Atividade/OperadoresLogicos/Execicio04.java
+++ src/Atividade/OperadoresLogicos/Execicio04.java
@@ -6,15 +6,13 @@ public class Execicio04 {
         //Declarar três variáveis boolean para verificar
         // se um número é positivo (num > 0), negativo (num < 0) ou zero (num == 0), exibindo os valores.
         int num1 = 10;
-        int num2 =  -1;
-        int num3 = 0;
 
         boolean valores = (num1 > 0);
-        boolean valores1 = (num2 > 0);
+        boolean valores1 = (num1 < 0);
         boolean valores2 = (num1 == 0);
 
-        System.out.println("Positivo :" + valores);
-        System.out.println("Negativo :" + valores1 );
-        System.out.println("Igual :" + valores2);
+        System.out.println("Numero é Positivo: " + valores);
+        System.out.println("Numero é Negativo: " + valores1 );
+        System.out.println("Numero não é = 0: " + valores2);
     }
 }
---
+++ src/Atividade/OperadoresLogicos/Execicio05.java
@@ -0,0 +1,10 @@
+package Atividade.OperadoresLogicos;
+
+public class Execicio05 {
+    public static void main(String[] args) {
+        //Declarar uma variável boolean aprovado e atribuir (nota >= 6), exibindo o valor.
+        int nota = 6;
+        boolean aprovado = (nota >= 6);
+        System.out.println("Aprovado nota : " +nota);
+    }
+}
