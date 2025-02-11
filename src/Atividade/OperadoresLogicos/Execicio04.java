c90ff219d5b071a8f9cde9f7ea0be7cd1a6a1916
Guizaoo <Guilhermexxzz54@gmail.com>
2025-02-11 11:51:50 -0300

    terceiro commit

---
+++ src/Atividade/OperadoresLogicos/Execicio04.java
@@ -0,0 +1,19 @@
+package Atividade.OperadoresLogicos;
+
+public class Execicio04 {
+    public static void main(String[] args) {
+        //Declarar três variáveis boolean para verificar
+        // se um número é positivo (num > 0), negativo (num < 0) ou zero (num == 0), exibindo os valores.
+        int num1 = 10;
+        int num2 =  -1;
+        int num3 = 0;
+
+        boolean valores = (num1 > 0);
+        boolean valores1 = (num2 > 0);
+        boolean valores2 = (num1 == 0);
+
+        System.out.println("Positivo :" + valores);
+        System.out.println("Negativo :" + valores1 );
+        System.out.println("Igual :" + valores2);
+    }
+}
